2360f87fe67a24927f1ebf0c560a94401203f94b
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-03-14 00:02:04 +0500

    Composite: add HeroUnit implementation

--- src/com/narxoz/rpg/composite/HeroUnit.java
+++ src/com/narxoz/rpg/composite/HeroUnit.java
@@ -1,7 +1,14 @@
 package com.narxoz.rpg.composite;
 
 public class HeroUnit extends UnitLeaf {
-    public HeroUnit(String name, int health, int attackPower) {
-        super(name, health, attackPower);
+
+    public HeroUnit(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+
+    @Override
+    public void attack(CombatNode target) {
+        System.out.println("Hero " + name + " strikes " + target.getName());
+        target.takeDamage(damage);
     }
 }
