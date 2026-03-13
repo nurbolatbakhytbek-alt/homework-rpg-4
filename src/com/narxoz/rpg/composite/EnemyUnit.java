a592330b227f0867d3b3b9b6597e59639e465e48
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-03-14 00:02:26 +0500

    Composite: add EnemyUnit implementation

--- src/com/narxoz/rpg/composite/EnemyUnit.java
+++ src/com/narxoz/rpg/composite/EnemyUnit.java
@@ -1,7 +1,14 @@
 package com.narxoz.rpg.composite;
 
 public class EnemyUnit extends UnitLeaf {
-    public EnemyUnit(String name, int health, int attackPower) {
-        super(name, health, attackPower);
+
+    public EnemyUnit(String name, int health, int damage) {
+        super(name, health, damage);
+    }
+
+    @Override
+    public void attack(CombatNode target) {
+        System.out.println("Enemy " + name + " attacks " + target.getName());
+        target.takeDamage(damage);
     }
 }
