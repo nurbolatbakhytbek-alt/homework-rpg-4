1ee0da59f7c2dae034a5696986d701f3e4c554b9
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-03-14 23:57:11 +0500

    Bridge: add Skill abstraction

--- src/com/narxoz/rpg/bridge/Skill.java
+++ src/com/narxoz/rpg/bridge/Skill.java
@@ -3,31 +3,12 @@ package com.narxoz.rpg.bridge;
 import com.narxoz.rpg.composite.CombatNode;
 
 public abstract class Skill {
-    private final String skillName;
-    private final int basePower;
-    private final EffectImplementor effect;
 
-    protected Skill(String skillName, int basePower, EffectImplementor effect) {
-        this.skillName = skillName;
-        this.basePower = basePower;
+    protected EffectImplementor effect;
+
+    public Skill(EffectImplementor effect) {
         this.effect = effect;
     }
 
-    public String getSkillName() {
-        return skillName;
-    }
-
-    public int getBasePower() {
-        return basePower;
-    }
-
-    public String getEffectName() {
-        return effect.getEffectName();
-    }
-
-    protected int resolvedDamage() {
-        return effect.computeDamage(basePower);
-    }
-
-    public abstract void cast(CombatNode target);
+    public abstract void use(CombatNode target);
 }
